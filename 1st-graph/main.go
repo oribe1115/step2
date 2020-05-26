0f12b005aaa2d5ea668393332465084fd3657f75
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-05-26 23:01:07 +0900

    複数回の実行まで

--- 1st-graph/main.go
+++ 1st-graph/main.go
@@ -3,14 +3,39 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	cmdStr := "cd ../1st/; go run main.go 3"
-	out, err := exec.Command("sh", "-c", cmdStr).Output()
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+	start := 3
+	end := 5
+	count := 5
+
+	runTimes := map[int][]int{}
+
+	for i := start; i <= end; i++ {
+		runTimes[i] = make([]int, count)
+		cmdStr := fmt.Sprintf("cd ../1st/; go run main.go %d", i)
+
+		fmt.Printf("%d: ", i)
+
+		for j := 0; j < count; j++ {
+			out, err := exec.Command("sh", "-c", cmdStr).Output()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			time, err := strconv.Atoi(strings.ReplaceAll(string(out), "\n", ""))
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			runTimes[i][j] = time
+			fmt.Printf("%d ", time)
+		}
+
+		fmt.Printf("\n")
 	}
-	fmt.Println(string(out))
+
 }
