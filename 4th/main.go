2e7adc57c462ca8838cf47fad1f69adafb793f79
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-05-27 15:36:10 +0900

    HistoryListの動作テスト

--- 4th/main.go
+++ 4th/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/oribe1115/step2/4th/lib"
+)
+
 func main() {
 
+	list := lib.InitHistoryList(4)
+	list.Print()
+	list.Cache("a")
+	list.Print()
+	list.Cache("b")
+	list.Print()
+	list.Cache("c")
+	list.Print()
+	list.Cache("d")
+	list.Print()
+
+	fmt.Println()
+
+	list.Cache("d")
+	list.Print()
+	list.Cache("a")
+	list.Print()
+	list.Cache("c")
+	list.Print()
+	list.Cache("a")
+	list.Print()
+	list.Cache("a")
+	list.Print()
+
+	fmt.Println()
+
+	list.Cache("e")
+	list.Print()
+	list.Cache("f")
+	list.Print()
 }
